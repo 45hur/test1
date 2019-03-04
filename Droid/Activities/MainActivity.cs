db14d5967f8feb36ed60290fa9179ecd028c973a
Ashur <Jan.Mikulasek@solarwinds.com>
2019-03-04 22:38:18 +0100

    Notify Android

--- Droid/Activities/MainActivity.cs
+++ Droid/Activities/MainActivity.cs
@@ -8,6 +8,8 @@ using Android.Support.V4.App;
 using Android.Support.V4.View;
 using Android.Support.Design.Widget;
 
+using System;
+
 namespace test1.Droid
 {
     [Activity(Label = "@string/app_name", Icon = "@mipmap/icon",
@@ -16,6 +18,10 @@ namespace test1.Droid
         ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : BaseActivity
     {
+        static readonly int NOTIFICATION_ID = 1000;
+        static readonly string CHANNEL_ID = "location_notification";
+        internal static readonly string COUNT_KEY = "count";
+
         protected override int LayoutResource => Resource.Layout.activity_main;
 
         ViewPager pager;
@@ -47,6 +53,11 @@ namespace test1.Droid
 
             SupportActionBar.SetDisplayHomeAsUpEnabled(false);
             SupportActionBar.SetHomeButtonEnabled(false);
+
+            CreateNotificationChannel();
+
+            var button = FindViewById<Button>(Resource.Id.MyButton);
+            button.Click += ButtonOnClick;
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -54,32 +65,86 @@ namespace test1.Droid
             MenuInflater.Inflate(Resource.Menu.top_menus, menu);
             return base.OnCreateOptionsMenu(menu);
         }
-    }
 
-    class TabsAdapter : FragmentStatePagerAdapter
-    {
-        string[] titles;
-
-        public override int Count => titles.Length;
-
-        public TabsAdapter(Context context, Android.Support.V4.App.FragmentManager fm) : base(fm)
+        void CreateNotificationChannel()
         {
-            titles = context.Resources.GetTextArray(Resource.Array.sections);
-        }
-
-        public override Java.Lang.ICharSequence GetPageTitleFormatted(int position) =>
-                            new Java.Lang.String(titles[position]);
-
-        public override Android.Support.V4.App.Fragment GetItem(int position)
-        {
-            switch (position)
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
             {
-                case 0: return BrowseFragment.NewInstance();
-                case 1: return AboutFragment.NewInstance();
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
             }
-            return null;
+
+            var name = Resources.GetString(Resource.String.channel_name);
+            var description = GetString(Resource.String.channel_description);
+            var channel = new NotificationChannel(CHANNEL_ID, name, NotificationImportance.Default)
+            {
+                Description = description
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
         }
 
-        public override int GetItemPosition(Java.Lang.Object frag) => PositionNone;
+        void ButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            // Pass the current button press count value to the next activity:
+            var valuesForActivity = new Bundle();
+            valuesForActivity.PutInt(COUNT_KEY, 0);
+
+            // When the user clicks the notification, SecondActivity will start up.
+            var resultIntent = new Intent(this, typeof(SecondActivity));
+
+            // Pass some values to SecondActivity:
+            resultIntent.PutExtras(valuesForActivity);
+
+            // Construct a back stack for cross-task navigation:
+            var stackBuilder = Android.Support.V4.App.TaskStackBuilder.Create(this);
+            stackBuilder.AddParentStack(Java.Lang.Class.FromType(typeof(SecondActivity)));
+            stackBuilder.AddNextIntent(resultIntent);
+
+            // Create the PendingIntent with the back stack:
+            var resultPendingIntent = stackBuilder.GetPendingIntent(0, (int)PendingIntentFlags.UpdateCurrent);
+
+            // Build the notification:
+            var builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                          .SetAutoCancel(true) // Dismiss the notification from the notification area when the user clicks on it
+                          .SetContentIntent(resultPendingIntent) // Start up this activity when the user clicks the intent.
+                          .SetContentTitle("SolarWinds Alert") // Set the title
+                          .SetNumber(1) // Display the count in the Content Info
+                          .SetSmallIcon(Resource.Drawable.notify_panel_notification_icon_bg) // This is the icon to display
+                          .SetContentText($"Something has happened."); // the message to display.
+
+            var notificationManager = NotificationManagerCompat.From(this);
+            notificationManager.Notify(NOTIFICATION_ID, builder.Build());
+        }
+
+        class TabsAdapter : FragmentStatePagerAdapter
+        {
+            string[] titles;
+
+            public override int Count => titles.Length;
+
+            public TabsAdapter(Context context, Android.Support.V4.App.FragmentManager fm) : base(fm)
+            {
+                titles = context.Resources.GetTextArray(Resource.Array.sections);
+            }
+
+            public override Java.Lang.ICharSequence GetPageTitleFormatted(int position) =>
+                                new Java.Lang.String(titles[position]);
+
+            public override Android.Support.V4.App.Fragment GetItem(int position)
+            {
+                switch (position)
+                {
+                    case 0: return BrowseFragment.NewInstance();
+                    case 1: return AboutFragment.NewInstance();
+                }
+                return null;
+            }
+
+            public override int GetItemPosition(Java.Lang.Object frag) => PositionNone;
+        }
     }
 }
---
+++ Droid/Activities/SecondActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace test1.Droid
+{
+    [Activity(Label = "Second Activity")]
+    public class SecondActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Get the count value passed to us from MainActivity:
+            var count = Intent.Extras.GetInt(MainActivity.COUNT_KEY, -1);
+
+            // No count was passed? Then just return.
+            //if (count <= 0)
+            //{
+            //    return;
+            //}
+
+            // Display the count sent from the first activity:
+            SetContentView(Resource.Layout.activity_add_item);
+            //var txtView = FindViewById<TextView>(Resource.Id.textView1);
+            //txtView.Text = $"You clicked the button in the previous activity.";
+        }
+    }
+}
